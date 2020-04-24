26a0e9e8bbb2c844ccf5c89588ed0c1af3b560f1
AMMAR1984 <54781984+AMMAR1984@users.noreply.github.com>
2020-04-24 12:56:23 +0100

    Add files via upload

---
+++ taj.sh
@@ -0,0 +1,79 @@
+clear
+echo " Number of boxes" ' distance' "the weight"
+read a     b
+echo $a'====>'$b
+echo " Number of boxes" ' distance' "the weight"
+read a1     b1
+c1=`expr $a + $a1`
+d1=`expr $b + $b1`
+echo $c1'====>'$d1
+echo " Number of boxes" ' distance' "the weight"
+read a2     b2
+c2=`expr $c1 + $a2`
+d2=`expr $d1 + $b2`
+echo $c2'====>'$d2
+echo " Number of boxes" ' distance' "the weight"
+read a3     b3
+c3=`expr $a + $a1 + $a2 + $a3`
+d3=`expr $b + $b1 + $b2 + $b3`
+echo $c3'====>'$d3
+echo " Number of boxes" ' distance' "the weight"
+read a4     b4
+c4=`expr $a + $a1 + $a2 + $a3 + $a4`
+d4=`expr $b + $b1 + $b2 + $b3 + $b4`
+echo $c4'====>'$d4
+echo " Number of boxes" ' distance' "the weight"
+read a5     b5
+c5=`expr $a + $a1 + $a2 + $a3 + $a4 + $a5`
+d5=`expr $b + $b1 + $b2 + $b3 + $b4 + $b5`
+echo $c5'====>'$d5
+echo " Number of boxes" ' distance' "the weight"
+read a6     b6
+c6=`expr $a + $a1 + $a2 + $a3 + $a4 + $a5 + $a6`
+d6=`expr $b + $b1 + $b2 + $b3 + $b4 + $b5 + $b6`
+echo $c6'====>'$d6
+echo " Number of boxes" ' distance' "the weight"
+read a7     b7
+c7=`expr $a + $a1 + $a2 + $a3 + $a4 + $a5 + $a6 + $a7`
+d7=`expr $b + $b1 + $b2 + $b3 + $b4 + $b5 + $b6 + $b7`
+echo $c7'====>'$d7
+echo " Number of boxes" ' distance' "the weight"
+read a8     b8
+c8=`expr $a + $a1 + $a2 + $a3 + $a4 + $a5 + $a6 + $a7 + $a8`
+d8=`expr $b + $b1 + $b2 + $b3 + $b4 + $b5 + $b6 + $b7 + $b8`
+echo $c8'====>'$d8
+echo " Number of boxes" ' distance' "the weight"
+read a9     b9
+c9=`expr $c8 + $a9`
+d9=`expr $d8 + $b9`
+echo $c9'====>'$d9
+echo " Number of boxes" ' distance' "the weight"
+read a10     b10
+c10=`expr $c9 + $a10`
+d10=`expr $d9 + $b10`
+echo $c10'====>'$d10
+echo " Number of boxes" ' distance' "the weight"
+read a11     b11
+c11=`expr $c10 + $a11`
+d11=`expr $d10 + $b11`
+echo $c11'====>'$d11
+echo " Number of boxes" ' distance' "the weight"
+read a12     b12
+c12=`expr $c11 + $a12`
+d12=`expr $d11 + $b12`
+echo $c12'====>'$d12
+echo " Number of boxes" ' distance' "the weight"
+read a13     b13
+c13=`expr $c12 + $a13`
+d13=`expr $d12 + $b13`
+echo $c13'====>'$d13
+echo " Number of boxes" ' distance' "the weight"
+read a14     b14
+c14=`expr $c13 + $a14`
+d14=`expr $d13 + $b14`
+echo $c14'====>'$d14
+echo " Number of boxes" ' distance' "the weight"
+read a15     b15
+c15=`expr $c14 + $a15`
+d15=`expr $d14 + $b15`
+echo $c15'====>'$d15
